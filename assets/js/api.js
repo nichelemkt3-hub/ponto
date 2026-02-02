d0837c7ccd55307790bd491fee54767d8d3de4fb
nichelemkt3-hub <nichelemkt3@gmail.com>
2026-02-02 14:54:33 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,9 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbwvZP_6-UFLnGb23viNtwf2GJ5aEaAK0zuj171SYOnFsEEwDyEqZBuRQRnea8-ejUYBKA/exec';
 
-async function enviarDados(payload) {
-  await fetch(API_URL, {
+async function api(payload) {
+  const res = await fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify(payload)
   });
+  return res.json();
 }
