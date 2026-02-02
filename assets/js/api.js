fbd5af7897b33b7ccca933bf4963277fe78d67d3
nichelemkt3-hub <nichelemkt3@gmail.com>
2026-02-02 17:41:34 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,15 +1,9 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbw6T084_OIjLNPikozHFm-5tA_WZAvRPC2Dyvw7WfdvuYJ2wdazgMszqY5naXWo5kyx/exec';
 
-async function api(payload) {
+async function postAPI(payload) {
   const res = await fetch(API_URL, {
     method: 'POST',
     body: JSON.stringify(payload)
   });
   return res.json();
-}
-
-function marcarKmConfirmado(botao) {
-  botao.classList.add('ok');
-  botao.innerText = 'KM Confirmado ✔';
-  botao.disabled = true;
 }
